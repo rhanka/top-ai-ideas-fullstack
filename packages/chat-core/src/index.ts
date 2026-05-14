5bc944d46e5136e493e947942fc160b639b9c510
antoinefa <fabien.antoine@m4x.org>
2026-05-14 12:06:16 -0400

    feat(chat-core): design MeshDispatchPort and MeshDispatchAdapter (BR14b Lot 10)

--- packages/chat-core/src/index.ts
+++ packages/chat-core/src/index.ts
@@ -2,6 +2,7 @@ export * from './checkpoint-port.js';
 export * from './message-port.js';
 export * from './stream-port.js';
 export * from './session-port.js';
+export * from './mesh-port.js';
 export * from './ports.js';
 export * from './types.js';
 export * from './runtime.js';
